edf9744e4f0a2aa84a83f3894b9a0b788d582549
solcrob <60452848+solcrob@users.noreply.github.com>
2020-02-05 18:35:21 +0100

    Update payment_acquirer.py

--- payment_payulatam_bi/models/payment_acquirer.py
+++ payment_payulatam_bi/models/payment_acquirer.py
@@ -4,17 +4,17 @@
 from werkzeug import urls
 import hashlib
 
-from odoo.addons.payment_payulatam_bi.controllers.main import PayuLatamController
+from odoo.addons.payment_payu.controllers.main import PayuLatamController
 
 from odoo import api, fields, models, _
 from odoo.exceptions import except_orm, Warning, RedirectWarning
 
 
-class PaymentAcquirerPayulatam(models.Model):
+class PaymentAcquirerPayu(models.Model):
     _inherit = 'payment.acquirer'
 
     def _get_payu_urls(self, environment):
-        """ PayuLatam URLs
+        """ Payu URLs
         """
         if environment == 'prod':
             return {'payu_form_url': 'https://secure.payu.com/api/v2_1/orders'}
@@ -23,16 +23,16 @@ class PaymentAcquirerPayulatam(models.Model):
 
     @api.model
     def _get_providers(self):
-        providers = super(PaymentAcquirerPayulatam, self)._get_providers()
-        providers.append(['payulatam', 'PayuLatam'])
+        providers = super(PaymentAcquirerPayu, self)._get_providers()
+        providers.append(['payu', 'Payu'])
         return providers
 
-    provider = fields.Selection(selection_add=[('payulatam', 'Payulatam')])
-    payulatam_merchantId = fields.Char(string='Merchant ID', required_if_provider='payulatam')
-    payulatam_accountId = fields.Char(string='Account ID', required_if_provider='payulatam')
-    payulatam_apiKey = fields.Char(string='API Key', required_if_provider='payulatam')
+    provider = fields.Selection(selection_add=[('payu', 'Payu')])
+    payu_merchantId = fields.Char(string='Merchant ID', required_if_provider='payu')
+    payu_accountId = fields.Char(string='Account ID', required_if_provider='payu')
+    payu_apiKey = fields.Char(string='API Key', required_if_provider='payu')
 
-    def _payulatam_generate_hashing(self, values):
+    def _payu_generate_hashing(self, values):
         data = '~'.join([
             values['ApiKey'],
             values['merchantId'],
@@ -44,8 +44,8 @@ class PaymentAcquirerPayulatam(models.Model):
         return m.hexdigest()
 
     @api.multi
-    def payulatam_form_generate_values(self, values):
-        payulatam_tx_values = dict(values)
+    def payu_form_generate_values(self, values):
+        payu_tx_values = dict(values)
         self.ensure_one()
         base_url = self.env['ir.config_parameter'].get_param('web.base.url')
         if self.environment == 'prod':
@@ -53,30 +53,30 @@ class PaymentAcquirerPayulatam(models.Model):
         else:
             test = 1
         temp_paylatam_tx_values = dict(
-            ApiKey=self.payulatam_apiKey,
-            merchantId=self.payulatam_merchantId,
-            accountId=self.payulatam_accountId,
+            ApiKey=self.payu_apiKey,
+            merchantId=self.payu_merchantId,
+            accountId=self.payu_accountId,
             referenceCode=values['reference'],
             description=values['reference'],
             amount=str(values['amount']),
             tax=0,
             currency=values['currency'] and values['currency'].name or '',
-            currency_sel=['ARS', 'BRL', 'CLP', 'COP', 'MXN', 'PEN', 'USD'],
+            currency_sel=['CZK', 'EUR', 'USD'],
             taxReturnBase=0,
             buyerEmail=values['partner_email'],
             test=test,
             responseUrl='%s' % urls.url_join(
-                base_url, PayuLatamController._return_url),
+                base_url, PayuController._return_url),
         )
-        temp_paylatam_tx_values['signature'] = self._payulatam_generate_hashing(temp_paylatam_tx_values)
-        payulatam_tx_values.update(temp_paylatam_tx_values)
-        return payulatam_tx_values
+        temp_pay_tx_values['signature'] = self._payu_generate_hashing(temp_pay_tx_values)
+        payu_tx_values.update(temp_pay_tx_values)
+        return payu_tx_values
 
     @api.multi
-    def payulatam_get_form_action_url(self):
+    def payu_get_form_action_url(self):
         self.ensure_one()
         return self._get_payu_urls(self.environment)['payu_form_url']
 
-PaymentAcquirerPayulatam()
+PaymentAcquirerPayu()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
